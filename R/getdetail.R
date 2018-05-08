612e2c665e34ee27eabaa2996d4ec7461e4784f0
bioguo <guokai8@gmail.com>
2018-05-08 13:47:06 -0500

    Update getdetail.R

--- R/getdetail.R
+++ R/getdetail.R
@@ -7,6 +7,7 @@
 getdetail<-function(rese,resd){
   if(!is.data.frame(resd)){
     resd=data.frame(gene=resd)
+    }
   gene<-strsplit(rese$GeneID,split=",")
   names(gene)<-rese$Annot
   gened<-data.frame("TERM"=rep(names(gene),times=unlist(lapply(gene,length))),"Annot"=rep(rese$Term,times=unlist(lapply(gene,length))),"GeneID"=unlist(gene),row.names=NULL)
