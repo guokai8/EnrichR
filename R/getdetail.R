d800b06d870f24c0690a4f67e3b77bf88c2c1ac3
bioguo <guokai8@gmail.com>
2018-05-08 13:42:12 -0500

    Update getdetail.R

--- R/getdetail.R
+++ R/getdetail.R
@@ -1,4 +1,5 @@
 #' Get detail information from enrichment results
+#' @importFrom tidyverse left_join
 #' @param rese Functional enrichment results
 #' @param resd DEG result or vector of input genes
 #' @export
@@ -6,7 +7,6 @@
 getdetail<-function(rese,resd){
   if(!is.data.frame(resd)){
     resd=data.frame(gene=resd)
-  }
   gene<-strsplit(rese$GeneID,split=",")
   names(gene)<-rese$Annot
   gened<-data.frame("TERM"=rep(names(gene),times=unlist(lapply(gene,length))),"Annot"=rep(rese$Term,times=unlist(lapply(gene,length))),"GeneID"=unlist(gene),row.names=NULL)
