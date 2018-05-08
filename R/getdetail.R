a01258c484e94a80b80def9ad88ca65d9482a8e6
bioguo <guokai8@gmail.com>
2018-05-08 13:40:44 -0500

    get details from enrichment result
    get details and return data.frame combine with input

---
+++ R/getdetail.R
@@ -0,0 +1,16 @@
+#' Get detail information from enrichment results
+#' @param rese Functional enrichment results
+#' @param resd DEG result or vector of input genes
+#' @export
+#' @author Kai Guo
+getdetail<-function(rese,resd){
+  if(!is.data.frame(resd)){
+    resd=data.frame(gene=resd)
+  }
+  gene<-strsplit(rese$GeneID,split=",")
+  names(gene)<-rese$Annot
+  gened<-data.frame("TERM"=rep(names(gene),times=unlist(lapply(gene,length))),"Annot"=rep(rese$Term,times=unlist(lapply(gene,length))),"GeneID"=unlist(gene),row.names=NULL)
+  gened$GeneID<-as.character(gened$GeneID)
+  res<-left_join(gened,resd,by=c("GeneID"="gene"))
+  return(res)
+}
