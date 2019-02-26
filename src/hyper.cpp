c389bf3ada150a7cc347ded6b514169a156d48dd
bioguo <guokai8@gmail.com>
2019-02-26 17:04:53 -0600

    Update hyper.cpp

--- src/hyper.cpp
+++ src/hyper.cpp
@@ -16,7 +16,7 @@ NumericVector hyper_bench_vector(NumericVector& xin,NumericVector& yin,double N,
   for(int i=0;i<xsize;++i){
     String tname=xnames[i];
     std::vector<double>xres;
-    xt=xin[tname]-1;
+    xt=xin[tname]-1.0;
     xres.push_back(xt);
     xres.push_back(yin[tname]);
     xres.push_back(N-yin[tname]);
