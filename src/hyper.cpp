d8034f534f17f71c65c496b0e4b8eb3bbc0ed410
bioguo <guokai8@gmail.com>
2019-02-26 16:55:40 -0600

    Update hyper.cpp

--- src/hyper.cpp
+++ src/hyper.cpp
@@ -10,12 +10,14 @@ double hyp_test( std::vector<double>& xa) {
 //[[Rcpp::export]]
 NumericVector hyper_bench_vector(NumericVector& xin,NumericVector& yin,double N,double n){
   int xsize=xin.size();
+  double xt;
   NumericVector res;
   StringVector xnames=xin.names();
   for(int i=0;i<xsize;++i){
     String tname=xnames[i];
     std::vector<double>xres;
-    xres.push_back((xin[tname]-1));
+    xt=xin[tname]-1;
+    xres.push_back(xt);
     xres.push_back(yin[tname]);
     xres.push_back(N-yin[tname]);
     xres.push_back(n);
